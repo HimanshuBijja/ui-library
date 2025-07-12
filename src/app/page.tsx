78a1c05c11589b6a58ce05071f200844b590a537
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-07-13 00:24:08 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,110 @@
 "use client";
 
-import { motion } from "motion/react";
+import { useEffect, useState } from "react";
+import namer from "color-namer";
 
 export default function Test1() {
+    const [colors, setColors] = useState<string[]>([]);
+
+    const [Clicked, setClicked] = useState(false);
+    useEffect(() => {
+        function randomRgb(): string {
+            const red = Math.floor(Math.random() * 255);
+            const green = Math.floor(Math.random() * 255);
+            const blue = Math.floor(Math.random() * 255);
+            return `rgb(${red},${green},${blue})`;
+        }
+        const generatedColors: string[] = [];
+        for (let i = 0; i < 5; i++) {
+            generatedColors.push(randomRgb());
+        }
+        setColors(generatedColors);
+        return () => {
+            setColors(generatedColors);
+        };
+    }, [Clicked]);
+
+    function isDarkColor(str: string) {
+        function isDark(r: number, g: number, b: number): boolean {
+            const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+            return yiq < 128;
+        }
+        const [r, g, b] = str
+            .slice(4, -1)
+            .split(",")
+            .map((s) => Number(s.trim()));
+        return isDark(r, g, b);
+    }
+
+    function rgbToHex(str: string) {
+        const [r, g, b] = str
+            .slice(4, -1)
+            .split(",")
+            .map((s) => Number(s.trim()));
+        return (
+            "#" +
+            [r, g, b]
+                .map((x) => x.toString(16).padStart(2, "0"))
+                .join("")
+                .toUpperCase()
+        );
+    }
+
+    function colorName(str: string) {
+        const result = namer(rgbToHex(str));
+        return result;
+    }
+
+    function generateNewColors() {
+        setClicked((prev) => !prev);
+    }
+
+    useEffect(() => {
+        window.focus();
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.code === "Space" || e.key === " ") {
+                e.preventDefault();
+                generateNewColors();
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, []);
+
     return (
-        <div className="flex h-screen w-full items-center justify-center bg-black overflow-hidden">
-            {/* <div className="h-100 w-100"> */}
-                
-            <motion.div
-                animate={{
-                    x: [0, 0, -200, -200, 0],
-                    y: [0, 200, 200, 0, 0],
-                    rotate: [0, 30, 60, 45, 90, 60, 30, 0],
-                    // scale: [1, 1.1, 0.95, 1],
-                    
-                    width: ["150px", "240px", "180px", "240px", "150px"],
-                    height: ["240px", "150px", "180px", "150px", "240px"],
-                }}
-                transition={{
-                    duration: 8,
-                    repeat: Infinity,
-                    
-                    // repeatType: "reverse",
-                }}
-                className="animate-three absolute flex blur-[50px] rounded-[50%] items-end justify-end bg-gradient-to-r from-pink-700 via-red-500 to bg-red-500 "
-            ></motion.div>
-                    {/* </div> */}
+        <div className="h-screen w-full grid grid-cols-5 relative">
+            <div className="absolute top-10 "></div>
+            {colors.map((color, index) => (
+                <div
+                    key={index}
+                    style={{ backgroundColor: color }}
+                    className={`h-full w-full flex items-baseline-last justify-center py-10`}
+                >
+                    <div
+                        className={`${
+                            isDarkColor(color) ? "text-white" : "text-black"
+                        } flex flex-col items-center justify-center`}
+                    >
+                        <div>{rgbToHex(color)}</div>
+                        <div className="text-xs w-fit mt-2  ">
+                            {colorName(color).ntc[0].name.toUpperCase()}
+                        </div>
+                    </div>
+                </div>
+            ))}
+
+            <div className="absolute top-10 right-1/2 translate-x-1/2 text-center">
+                <div className="p-4 bg-white/12 backdrop-blur-[12px] rounded-2xl outline-2 outline-white/32 ">
+                    Press Spacebar or this button to generate new colors
+                </div>
+                <button
+                    className=" outline-4 px-8 py-1 outline-white/32 rounded-2xl bg-white/80 backdrop-blur-[100px] mt-4"
+                    onClick={() => setClicked(!Clicked)}
+                >
+                    Generate
+                </button>
+            </div>
         </div>
     );
 }
